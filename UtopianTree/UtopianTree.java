4e96ffd4800f254b21c20feb6305df91bc64f151
ZSGS-Vipin <112948342+ZSGS-Vipin@users.noreply.github.com>
2022-09-12 15:37:34 +0530

    Update UtopianTree.java

--- UtopianTree/UtopianTree.java
+++ UtopianTree/UtopianTree.java
@@ -1,1 +1,22 @@
+public class UtopianTree {
+    public static void main(String[] args) {
+       int ans =  utopianTree(3);
+        System.out.println(ans);
+    }
+    public static int utopianTree(int n) {
+        int maxHeight = 1;
+        if(n==0)
+            return maxHeight;
+        for(int i = 1; i <=n;i++){
+            if(i%2!=0)
+                maxHeight *= 2;
+            else
+                maxHeight+=1;
+        }
+        // Write your code here
+        return maxHeight;
+
+    }
+
+}
 
